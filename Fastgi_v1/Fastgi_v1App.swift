3fc6b2f0a4964b3fb04902df2d8efea61f345e59
omar <you@gmail.com>
2020-10-26 17:22:58 -0400

    Initial Commit

---
+++ Fastgi_v1/Fastgi_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Fastgi_v1App.swift
+//  Fastgi_v1
+//
+//  Created by Hegaro on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Fastgi_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
